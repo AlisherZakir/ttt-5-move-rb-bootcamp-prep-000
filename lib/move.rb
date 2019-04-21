84f0928e7aa1cad6f1b4cef0e7c4bdef1ac52b46
Alisher Zakir <a3zakir@gmail.com>
2019-04-21 16:01:19 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,10 +8,9 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
-  return input.to_i - 1
+  return input.to_i! - 1
 end
 def move(board, index, character = "X")
-  puts "Welcome to Tic Tac Toe!"
   board[index] = character
   return board
 end
